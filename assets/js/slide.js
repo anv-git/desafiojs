93456835a5cefd6471cb6ba7d13147a94f11af5d
anv-git <anvcarolina@outlook.com>
2021-08-04 20:22:13 -0300

    WIP/modify

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,10 +1,11 @@
 
-  const menu = document.querySelector(".menu__navegation");
+  const menu = document.querySelector(".menu__navegation--position");
   const nav = document.querySelector(".menu__navegation--anchor");
   
   
   menu.addEventListener('click', () => {
     nav.classList.toggle('slide');
+    nav.style.margin = 0;
     document.querySelector(".menu__navegation--position").classList.toggle("active");
     });
     

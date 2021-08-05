3553f6d56a3546b013cae649d6e0b81840dafeff
anv-git <anvcarolina@outlook.com>
2021-08-05 03:15:23 -0300

    WIP/accordion

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -1,4 +1,3 @@
-
 const menu = document.querySelector(".menu__navegation--position");
 const nav = document.querySelector(".menu__navegation--anchor");
 
@@ -8,4 +7,12 @@ menu.addEventListener('click', () => {
   nav.style.margin = 0;
   document.querySelector(".menu__navegation--position").classList.toggle("active");
   });
-  
+
+
+  const collapse = document.getElementsByClassName('collapse__box');
+
+  for (i = 0; i<collapse.length; i++){
+      collapse[i].addEventListener('click', function(){
+          this.classList.toggle('active')
+      })
+  }
